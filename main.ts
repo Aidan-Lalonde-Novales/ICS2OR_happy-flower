1fef3c0dc1dadfd21722ebe1ea1880686432ef15
Aidan Lalonde-Novales <71395339+Aidan-Lalonde-Novales@users.noreply.github.com>
2020-11-17 10:04:03 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,48 @@
+// Created by Aidan L-N
+// Created on the 17th of November, 2020
+// 
+// Code blocks that cause bees to leave a flower.
+let projectile: Sprite = null
+scene.setBackgroundColor(9)
+let mySprite = sprites.create(img`
+    . . . . . . . . 
+    . . . . . . . . 
+    . . . . . . . . 
+    . . . . . . . . 
+    . b b d d b b . 
+    b 1 1 3 3 1 1 b 
+    b 1 3 5 5 3 1 b 
+    b d 3 5 5 3 d b 
+    c 1 1 d d 1 1 c 
+    c d 1 d d 1 d c 
+    . c c 7 6 c c . 
+    . . 6 7 6 . . . 
+    . . 6 6 8 8 8 6 
+    . . 6 8 7 7 7 6 
+    . . 8 7 7 7 6 . 
+    . . 8 8 8 6 . . 
+    `, SpriteKind.Player)
+game.onUpdateInterval(1000, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . 1 1 . . . . 
+        . . . . . . . . . 1 1 . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . f 5 f 5 f 5 f 5 f . . . . 
+        . . f 5 5 f 5 f 5 f 5 f f . . . 
+        . f 5 f 5 5 f 5 f 5 f 5 f f . . 
+        . f 5 5 5 5 f 5 1 1 f 5 f f f f 
+        . f 5 5 5 5 f 5 f 1 1 5 f f f . 
+        . . f 5 5 5 f 5 f 5 f 5 f f . . 
+        . . . f 5 5 f 5 f 5 f 5 f . . . 
+        . . . . f f f f f f f f . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, randint(-25, 25), randint(-25, 25))
+    projectile.lifespan = 3000
+    if (projectile.vx > 0) {
+        projectile.image.flipX()
+    }
+})
